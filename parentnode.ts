39db7cf5e542daa60c75f641ec4fcebdbfff6d13
Shoaib-Programmer <shoaibnigam422@gmail.com>
2025-08-28 13:08:56 +0000

    Added ParentNode

---
+++ parentnode.ts
@@ -0,0 +1,24 @@
+import { HTMLNode } from "./htmlnode";
+
+export class ParentNode extends HTMLNode {
+  constructor(
+    public tag: string,
+    public children: HTMLNode[],
+    public props: Record<string, string> | undefined
+  ) {
+    super(tag, undefined, children, props);
+  }
+
+  public toHTML() {
+    if (!this.tag) {
+        throw new Error("The tag must be present")
+    }
+
+    if (!this.children) {
+        throw new Error("Children must be present for a Parent")
+    }
+
+    const childrenHTML = this.children.map(child => child.toHTML()).join("");
+    return `<${this.tag} ${super.propsToHTML()}>${childrenHTML}</${this.tag}>`;
+  }
+}
