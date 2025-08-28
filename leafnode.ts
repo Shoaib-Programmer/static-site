f9d739feda656b8be893a6436b3410a559ff0833
Shoaib-Programmer <shoaibnigam422@gmail.com>
2025-08-28 13:08:36 +0000

    Added LeafNode

---
+++ leafnode.ts
@@ -0,0 +1,23 @@
+import { HTMLNode } from "./htmlnode";
+
+export class LeafNode extends HTMLNode {
+  constructor(
+    public tag: string,
+    public value: string,
+    public props: Record<string, string> | undefined
+  ) {
+    super(tag, value, undefined, props);
+  }
+
+  public toHTML() {
+    if (!this.value) {
+      throw new Error("Value must be present for a LeafNode");
+    }
+
+    if (!this.tag) {
+      return this.value;
+    }
+
+    return `<${this.tag} ${super.propsToHTML()}>${this.value}</${this.tag}>`
+  }
+}
