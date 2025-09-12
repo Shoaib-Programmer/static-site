92d3c3f9cd3e1aac9c1d99bfd68fe5bef5ff76e6
Shoaib-Programmer <shoaibnigam422@gmail.com>
2025-09-12 16:14:54 +0000

    Fixed minor error

--- leafnode.ts
+++ leafnode.ts
@@ -10,12 +10,20 @@ export class LeafNode extends HTMLNode {
     }
 
     public toHTML() {
-        if (!this.value) {
+        // For self-closing tags like img, empty value is allowed
+        const selfClosingTags = ['img', 'br', 'hr', 'input', 'meta', 'link'];
+        
+        if (!this.value && (!this.tag || !selfClosingTags.includes(this.tag))) {
             throw new Error("Value must be present for a LeafNode");
         }
 
         if (!this.tag) {
             return this.value;
+        }
+
+        // Handle self-closing tags
+        if (selfClosingTags.includes(this.tag)) {
+            return `<${this.tag} ${super.propsToHTML()} />`;
         }
 
         return `<${this.tag} ${super.propsToHTML()}>${this.value}</${this.tag}>`;
